2a2a0289bbe94cefc25e91c8bbd7a486fa7ac291
templarofdaoism <templarofdaoism@protonmail.com>
2019-03-01 21:25:18 -0800

    writing model

--- model.py
+++ model.py
@@ -1,2 +1,8 @@
 import tensorflow as tf
-    
+
+#unknown basis under which x is sparse
+sparseBasis = tf.get_variable("U", [10, 10])
+
+# sensing matrix
+sensingMatrix = tf.constant(-1.0, shape=[2, 3])
+
